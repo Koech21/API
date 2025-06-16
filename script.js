ef0556fb8b0965a1325273b0acbbb4471377f16c
Ryan Kipkoech <kipkoechRyan21@gmail.com>
2025-06-16 16:44:30 +0300

    API

---
+++ script.js
@@ -0,0 +1,54 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'aaa82a7849msh38bac61ac13aa96p1a2038jsn79f1fe00953e',
+		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+	}
+};
+
+ async function serachAndDisplaymovies(title){
+try {
+	const response = await fetch(`https://imdb236.p.rapidapi.com/api/imdb/search?originalTitle=${title}&type=movie&genre=Drama&rows=25&sortOrder=ASC&sortField=id`, options);
+	const result = await response.json();
+	console.log(result);
+    const moviesList = document.getElementById("results-list")
+    moviesList.innerHTML=""
+    result.results.forEach((movie)=>{
+        const movieElement = document.createElement("li");
+        movieElement.className= "movie-item";
+        movieElement.innerHTML=`
+          <img src="${movie.primaryImage}" alt="${movie.primaryTitle}">
+          <h3><span>${movie.type}</span>${movie}</h3>
+          <p>${movie.description?.slice(0,200)|| "No description available" }</p>
+          ${
+            movie.contentRating
+            ? `<p> Content Rating: ${movie.contentRating}</p>`
+            : ""
+          }
+        `;
+        moviesList.appendChild(movieElement);
+    })
+} catch (error) {
+	console.error(error);
+    const moviesList = document.getElementById("results-list")
+    moviesList.innerHTML=`
+       <li class="error">Error loading movies. please try again later</li>
+    `
+}
+}
+
+
+document.querySelector("form").addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+     const searchTerm = document.querySelector("#title").value;
+  if (searchTerm.trim() === "") {
+    alert("Please search for something before submiting the form.");
+    return;
+  }
+
+    serachAndDisplaymovies(searchTerm)
+    document.querySelector("#title").value = "";
+})
+//task - ammend the code to include a drop down menu for a search category
